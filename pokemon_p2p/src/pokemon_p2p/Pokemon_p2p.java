e6889a3c7df1572e6e5b33c2f0c898ae5e0fb803
SorletoDomenico <dome.sorleto2003@gmail.com>
2021-12-03 13:33:36 +0100

    Inizio

---
+++ pokemon_p2p/src/pokemon_p2p/Pokemon_p2p.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon_p2p;
+
+/**
+ *
+ * @author sorleto_domenico
+ */
+public class Pokemon_p2p {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
