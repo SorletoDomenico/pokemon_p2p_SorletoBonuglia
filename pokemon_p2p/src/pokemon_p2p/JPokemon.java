c4b35b57c0a4f675abc18ed092138494353418df
SorletoDomenico <dome.sorleto2003@gmail.com>
2021-12-17 13:06:40 +0100

    classe JPokemon
    vari get e set + toString

--- pokemon_p2p/src/pokemon_p2p/JPokemon.java
+++ pokemon_p2p/src/pokemon_p2p/JPokemon.java
@@ -10,5 +10,83 @@ package pokemon_p2p;
  * @author sorleto_domenico
  */
 public class JPokemon {
+
+   
+    Integer id;
+    String name;
+    String type1;
+    String type2;
+    String HP;
+    String description;
+    String sprite;
+    String hires;
     
+     public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+    public String getHP() {
+        return HP;
+    }
+
+    public void setHP(String HP) {
+        this.HP = HP;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(String sprite) {
+        this.sprite = sprite;
+    }
+
+    public String getHires() {
+        return hires;
+    }
+
+    public void setHires(String hires) {
+        this.hires = hires;
+    }
+    
+    public String toString(){
+    return name+HP;
+    
+    }
 }
