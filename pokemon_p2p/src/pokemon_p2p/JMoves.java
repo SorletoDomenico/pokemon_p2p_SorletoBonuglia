7d2249189b190b5b6abad5ebf93e27319c8f036e
BonugliaGabriele <bonuglia.gabriele@gmail.com>
2021-12-21 15:28:16 +0100

    get e set JMoves
    +tostring

--- pokemon_p2p/src/pokemon_p2p/JMoves.java
+++ pokemon_p2p/src/pokemon_p2p/JMoves.java
@@ -10,10 +10,65 @@ package pokemon_p2p;
  * @author sorleto_domenico
  */
 public class JMoves {
+
+    
     Integer accurancy;
     String ename;
     Integer id;
     Integer power;
     Integer pp;
     String type;
+    
+  public Integer getAccurancy() {
+        return accurancy;
+    }
+
+    public void setAccurancy(Integer accurancy) {
+        this.accurancy = accurancy;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    public Integer getPp() {
+        return pp;
+    }
+
+    public void setPp(Integer pp) {
+        this.pp = pp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String toString(){
+    
+    return ename+power;
+    }
 }
